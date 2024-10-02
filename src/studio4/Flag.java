6dbaaa6264b4cd21f1794f46c4e04c1355135062
maddycrawford <maddycrawford@localhost>
2024-10-02 15:44:27 -0500

    studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,42 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		//background
+		StdDraw.setPenColor(Color.MAGENTA);
+		StdDraw.filledRectangle(0, 0, 1, 1); 
+		
+		//head
+		StdDraw.setPenColor(Color.GRAY);
+		StdDraw.filledCircle(.5, .5, .25);
+		
+		//ears
+		StdDraw.filledPolygon(new double[] {.55, .65, .625}, new double[] {.7, .7, .9}); 
+		StdDraw.filledPolygon(new double[] {.45, .35, .375}, new double[] {.7, .7, .9}); 
+		
+		//eyes 
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledCircle(.6, .55, .05);
+		StdDraw.filledCircle(.4, .55, .05);
+
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledCircle(.6, .55, .025);
+		StdDraw.filledCircle(.4, .55, .025);
+
+
+		//nose 
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledPolygon(new double[] {.475, .525, .5}, new double[] {.425, .425, .475}); 
+
+
+		//whiskers
+		StdDraw.line(.53, .45, .6, .47);
+		StdDraw.line(.53, .45, .6, .45);
+		StdDraw.line(.53, .45, .6, .43);
+
+		StdDraw.line(.47, .45, .4, .47);
+		StdDraw.line(.47, .45, .4, .45);
+		StdDraw.line(.47, .45, .4, .43);
+
+		
 	}
 }
--- src/studio4/NorwayMotherOfAllFlags.java
+++ src/studio4/NorwayMotherOfAllFlags.java
@@ -1,5 +1,7 @@
 package studio4;
 
+import java.awt.Color;
+
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
@@ -29,28 +31,95 @@ public class NorwayMotherOfAllFlags {
 	}
 
 	public static void france() {
+		StdDraw.clear();
+		StdDraw.setXscale(0, 22);
+		StdDraw.setYscale(0, 16);
 		
-		// TODO
+		StdDraw.setPenColor(Color.BLUE);
+	    StdDraw.filledRectangle(22.0 / 6, 8, 22.0 / 6, 8);
+	    
+	    // White stripe
+	    StdDraw.setPenColor(Color.WHITE);
+	    StdDraw.filledRectangle(22.0 / 2, 8, 22.0 / 6, 8);
+	    
+	    // Red stripe
+	    StdDraw.setPenColor(Color.RED);
+	    StdDraw.filledRectangle(22.0 * 5 / 6, 8, 22.0 / 6, 8);
 	}
+	
 
 	public static void indonesia() {
+		StdDraw.clear();
+		StdDraw.setXscale(0, 22);
+		StdDraw.setYscale(0, 16);
 		
-		// TODO
+		//white half
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledRectangle(11, 12, 11, 4);
+		
+		
+		//red half
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(11, 4, 11, 4);
+		
 	}
 
 	public static void netherlands() {
+		StdDraw.clear();
+		StdDraw.setXscale(0, 22);
+		StdDraw.setYscale(0, 16);
 		
-		// TODO
+		//red stripe 
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(11, 12.75, 11, 2.66);
+
+		//white stripe 
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledRectangle(11, 7.45, 11, 2.66);
+
+
+		//blue stripe
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle(11, 2.15, 11, 2.66);
+
 	}
 
 	public static void poland() {
 		
-		// TODO
-	}
+		StdDraw.clear();
+		StdDraw.setXscale(0, 22);
+		StdDraw.setYscale(0, 16);
+		
+		//white half
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledRectangle(11, 12, 11, 4);
+		
+		
+		//red half
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(11, 4, 11, 4);	}
 
 	public static void thailand() {
+		StdDraw.clear();
+		StdDraw.setXscale(0, 22);
+		StdDraw.setYscale(0, 16);
 		
-		// TODO
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(11, 16.8, 11, 22/12);	
+		
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledRectangle(11, 14.2, 11, 22/12);	
+		
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle(11, 8, 11, 22/6);	
+		
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledRectangle(11, 3.9, 11, 22/12);	
+		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(11, 1.3, 11, 22/12);
+		
+	
 	}
 
 	public static void finland() {
